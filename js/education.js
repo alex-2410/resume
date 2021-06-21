c2647dbc007f382ff1a120582f35614864a642e6
= <ritvikasonawane1234@gmail.com>
2021-06-21 20:13:17 +0530

    Added loader to education

--- js/education.js
+++ js/education.js
@@ -1,8 +1,9 @@
+// onClick
 function onClick(clicked_id){
     if (clicked_id=="index") {
         setTimeout(()=> { location.href = "../index.html"},1000);    
     }
     else {
-        setTimeout(()=> { location.href = clicked_id+".html"},1000);
+        setTimeout(()=> { location.href = clicked_id + ".html"},1000);
     }
 }
